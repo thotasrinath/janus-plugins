8851a9e58448362217b70448df19e08e6072a709
sthota <sthota@cloudleaf.io>
2022-08-08 15:52:15 +0800

    Correction in schema creation

--- src/main/java/io/cdap/plugin/janus/examples/JanusGraphSchemaMgmtApp.java
+++ src/main/java/io/cdap/plugin/janus/examples/JanusGraphSchemaMgmtApp.java
@@ -1,9 +1,12 @@
 package io.cdap.plugin.janus.examples;
 
 
+import io.cdap.plugin.janus.common.JanusCustomConfiguration;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.apache.commons.configuration2.Configuration;
 import org.apache.commons.configuration2.ex.ConfigurationException;
@@ -29,29 +32,26 @@ public class JanusGraphSchemaMgmtApp {
 
     public void configureSchema() throws ConfigurationException, IOException {
 
-        Map<String, Object> mapConfig = new HashMap<String, Object>();
-        mapConfig.put("gremlin.remote.remoteConnectionClass",
-                "org.apache.tinkerpop.gremlin.driver.remote.DriverRemoteConnection");
-        mapConfig.put("gremlin.remote.driver.sourceName", "g");
 
+        final JanusCustomConfiguration janusCustomConfiguration = new JanusCustomConfiguration();
 
-        Configuration janusConf = ConfigurationUtil.loadMapConfiguration(mapConfig);
-
-        Configuration configuration = ConfigurationUtil.createBaseConfiguration();
-        configuration.addProperty("hosts", "localhost");
-        configuration.addProperty("port", String.valueOf(8182));
-        configuration.addProperty("serializer.className",
+        janusCustomConfiguration.addPropertyConfigDirect("hosts",
+                Arrays.stream("localhost".split(",")).collect(Collectors.toList()));
+        janusCustomConfiguration.addPropertyConfigDirect("port", "8182");
+        janusCustomConfiguration.addPropertyConfigDirect("serializer.className",
                 "org.apache.tinkerpop.gremlin.driver.ser.GryoMessageSerializerV3d0");
+        janusCustomConfiguration.addPropertyConfigDirect("serializer.config.ioRegistries",
+                Arrays.stream("org.janusgraph.graphdb.tinkerpop.JanusGraphIoRegistry".split(",")).collect(
+                        Collectors.toList())); // (e.g. [ org.janusgraph.graphdb.tinkerpop.JanusGraphIoRegistry) ]
+        janusCustomConfiguration.addPropertyConfigDirect("gremlin.remote.remoteConnectionClass",
+                "org.apache.tinkerpop.gremlin.driver.remote.DriverRemoteConnection");
+        janusCustomConfiguration.addPropertyConfigDirect("gremlin.remote.driver.sourceName",
+                "g");
 
-        Map<String, Object> serializerConfig = new HashMap<String, Object>();
-        mapConfig.put("ioRegistries", "org.janusgraph.graphdb.tinkerpop.JanusGraphIoRegistry");
-
-
-        configuration.addProperty("serializer.config", serializerConfig);
 
         // using the remote driver for schema
         try {
-            Cluster cluster = Cluster.open(configuration);
+            Cluster cluster = Cluster.open(janusCustomConfiguration);
             Client client = cluster.connect();
             LOGGER.info("creating schema");
             // get the schema request as a string
@@ -63,6 +63,7 @@ public class JanusGraphSchemaMgmtApp {
             futureList.map(Result::toString).forEach(System.out::println);
             client.close();
             cluster.close();
+
             System.out.println("Done");
         } catch (Exception e) {
             e.printStackTrace();
@@ -71,28 +72,32 @@ public class JanusGraphSchemaMgmtApp {
 
     }
 
+
     protected String createSchemaRequest() {
         final StringBuilder s = new StringBuilder();
 
         s.append("JanusGraphManagement management = graph.openManagement(); ");
         s.append("boolean created = false; ");
 
+        // naive check if the schema was previously created
+        s.append(
+                "if (management.getRelationTypes(RelationType.class).iterator().hasNext()) { management.rollback(); created = false; } else { ");
+
 // properties
+        s.append("PropertyKey id = management.makePropertyKey(\"id\").dataType(String.class).make(); ");
         s.append("PropertyKey ident = management.makePropertyKey(\"ident\").dataType(String.class).make(); ");
         s.append("PropertyKey type = management.makePropertyKey(\"type\").dataType(String.class).make(); ");
         s.append("PropertyKey name = management.makePropertyKey(\"name\").dataType(String.class).make(); ");
-        // s.append("PropertyKey airport_name = management.makePropertyKey(\"airport_name\").dataType(String.class).make(); ");
         s.append(
                 "PropertyKey airport_wikipedia_link = management.makePropertyKey(\"airport_wikipedia_link\").dataType(String.class).make(); ");
         s.append(
                 "PropertyKey country_code = management.makePropertyKey(\"country_code\").dataType(String.class).make(); ");
-        //  s.append("PropertyKey country_name = management.makePropertyKey(\"country_name\").dataType(String.class).make(); ");
         s.append(
                 "PropertyKey country_continent = management.makePropertyKey(\"country_continent\").dataType(String.class).make(); ");
         s.append(
                 "PropertyKey country_wikipedia_link = management.makePropertyKey(\"country_wikipedia_link\").dataType(String.class).make(); ");
-        s.append("PropertyKey country_id = management.makePropertyKey(\"country_id\").dataType(String.class).make(); ");
-        s.append("PropertyKey airport_id = management.makePropertyKey(\"airport_id\").dataType(String.class).make(); ");
+        s.append("PropertyKey countryId = management.makePropertyKey(\"countryId\").dataType(String.class).make(); ");
+        s.append("PropertyKey airportId = management.makePropertyKey(\"airportId\").dataType(String.class).make(); ");
 
 
         // vertex labels
@@ -108,8 +113,7 @@ public class JanusGraphSchemaMgmtApp {
         // s.append("management.buildIndex(\"nameIndex\", Vertex.class).addKey(name).buildCompositeIndex(); ");
 
         // mixed indexes
-
-        s.append("management.commit(); created = true; ");
+        s.append("management.commit(); created = true; }");
 
         return s.toString();
     }
